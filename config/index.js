fad48e79b6984b3589563c5ea184cdf5f4568dc4
bernovikes <bernovikes@hotmail.com>
2018-11-06 14:58:44 +0800

    init

--- config/index.js
+++ config/index.js
@@ -1,8 +1,12 @@
 'use strict'
 const path = require('path')
 module.exports = {
+    dev: {
+        assetsPublicPath: '/',
+    },
     build: {
         assetsRoot: path.resolve(__dirname, '../dist'),
-        assetsSubDirectory: 'static'
+        assetsSubDirectory: 'static',
+        assetsPublicPath: '/',
     }
 }
